795100f93c64886ba3bfe9e4ce142d82fa9a7f8e
Aruu Laptop Git <aruulaptopgit@gmail.com>
2020-10-23 23:44:01 +0600

    Updates

---
+++ precache-manifest.f007610b4652c82bc22a48eeb7532a92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b388518310324d752488bc92df7cf1e8",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c696513ae7cb8fde6105",
+    "url": "/weather/static/js/2.e375e23f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/weather/static/js/2.e375e23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88a54d3b5cf2c20b01c",
+    "url": "/weather/static/js/main.f1989476.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
